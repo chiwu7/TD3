2443eefc4dda845065dbff1828d233e02cc9ddce
Tristan <94827589+chiwu7@users.noreply.github.com>
2022-11-15 08:13:11 +0000

    Initial commit

---
+++ Exo_1/PointA.java
@@ -0,0 +1,20 @@
+package Exo_1;
+
+public class PointA extends Point {
+
+  public void Afficher() {
+    System.out.println("Les coordonnées du point sont (" + getX() + "," + getY() + ")");
+  }
+  
+}
+//Question 3 : 
+
+// X et Y ne serait pas accessible.
+
+/*
+public static void main(String[] args) {
+    PointA simon = new PointA();
+    simon.initialise(7,3);
+    simon.Afficher();
+  }
+*/
---
+++ Main.java
@@ -0,0 +1,11 @@
+import Exo_1.PointA;
+
+class Main {
+  
+  public static void main(String[] args) {
+    PointA simon = new PointA();
+    simon.initialise(7,3);
+    simon.Afficher();
+  }
+  
+}
